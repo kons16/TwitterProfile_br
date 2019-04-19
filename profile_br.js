c4047b1faaef5518ebdac0971d65488a3fed502a
kons16 <ks1216r@gmail.com>
2019-04-19 22:54:21 +0900

    /も追加

--- profile_br.js
+++ profile_br.js
@@ -1,3 +1,4 @@
 var profile_collection = document.getElementsByClassName('ProfileHeaderCard-bio u-dir');;
 var text = profile_collection[0].innerHTML;
-profile_collection[0].innerHTML = text.replace(' ', '<br/>');
+profile_collection[0].innerHTML = text.replace(' ', '<br/>')
+                                      .replace('/', '<br/>');
