af41a4bdabfd2861e629660fc678d277a6c74d40
kons16 <ks1216r@gmail.com>
2019-04-19 22:33:51 +0900

    スクリプトファイルとmanifestの追加

---
+++ profile_br.js
@@ -0,0 +1,3 @@
+var profile_collection = document.getElementsByClassName('ProfileHeaderCard-bio u-dir');;
+var text = profile_collection[0].innerHTML;
+profile_collection[0].innerHTML = text.replace(' ', '<br/>');
