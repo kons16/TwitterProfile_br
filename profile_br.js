9a1c349afda6e0212eb5af2b5ea4d820831b7653
kons16 <ks1216r@gmail.com>
2019-04-19 22:56:17 +0900

    修正

--- profile_br.js
+++ profile_br.js
@@ -1,4 +1,4 @@
-var profile_collection = document.getElementsByClassName('ProfileHeaderCard-bio u-dir');;
+var profile_collection = document.getElementsByClassName('ProfileHeaderCard-bio u-dir');
 var text = profile_collection[0].innerHTML;
 profile_collection[0].innerHTML = text.replace(' ', '<br/>')
                                       .replace('/', '<br/>');
